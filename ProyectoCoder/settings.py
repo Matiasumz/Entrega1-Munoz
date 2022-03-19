5cefcc8cb5da1bb699b00716ed44d447c3216813
Matiasumz <matiasumz96@gmail.com>
2022-03-19 00:47:06 -0300

    Se crearon templates, se registro la app en installed app, se importo dotenv y modifique secret key y debug

--- ProyectoCoder/settings.py
+++ ProyectoCoder/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "index",
 ]
 
 MIDDLEWARE = [
@@ -59,7 +60,7 @@ ROOT_URLCONF = 'ProyectoCoder.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [r"C:\Users\matia\OneDrive\Escritorio\ProyectoCoder\index\plantillas\plantilla.html"],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- index/urls.py
+++ index/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
-from .views import index,plantilla
-
+from .views import index , plantilla
 urlpatterns = [
     path("",index, name="index"),
     path("plantilla/",plantilla,name="plantilla"),
