9e056589505c1cb94bdfe7c1c45bf1284008f446
Matiasumz <matiasumz96@gmail.com>
2022-03-20 15:56:11 -0300

    Se creo la app profesionales,con sus respectivos modelos y se registro la app en settings del proyecto.

---
+++ profesionales/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class Cerrajero(models.Model):
+    nombre = models.CharField(max_length=20)
+    apellido = models.CharField(max_length=20)
+    desempleado = models.BooleanField()
+    
+class Futbolista(models.Model):
+    nombre = models.CharField(max_length=20)
+    apellido = models.CharField(max_length=20)
+    club = models.CharField(max_length=20)
+    
+    
