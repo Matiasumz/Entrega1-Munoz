e2d2c2c19a96f42801522bee0fbb93eb84f7ef8c
Matiasumz <matiasumz96@gmail.com>
2022-03-20 16:20:32 -0300

    Se creo el modelo de Profesor y se implemento a la base de datos

--- profesionales/admin.py
+++ profesionales/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from profesionales.models import Cerrajero, Futbolista, Profesor
+
 # Register your models here.
+
+admin.site.register(Cerrajero)
+admin.site.register(Futbolista)
+admin.site.register(Profesor)
--- profesionales/models.py
+++ profesionales/models.py
@@ -6,9 +6,15 @@ class Cerrajero(models.Model):
     apellido = models.CharField(max_length=20)
     desempleado = models.BooleanField()
     
+    
 class Futbolista(models.Model):
     nombre = models.CharField(max_length=20)
     apellido = models.CharField(max_length=20)
     club = models.CharField(max_length=30)
     
+    
+class Profesor(models.Model):
+    nombre = models.CharField(max_length=20)
+    apellido = models.CharField(max_length=20)
+    curso = models.CharField(max_length=20)
     
