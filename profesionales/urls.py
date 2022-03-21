7f288ce23be2cf5df42ce5a5ac26e7bc65ae4456
Matiasumz <matiasumz96@gmail.com>
2022-03-20 21:36:55 -0300

    Se creo el formullario con su vista, y el redirect a index, y el superuser

---
+++ profesionales/urls.py
@@ -0,0 +1,9 @@
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("cerrajero/crear/",views.crear_cerrajero, name= "crear_cerrajero"),
+    #path("",name= "buscar_cerrajero"),
+    #path("",name= "crear_profesor")
+]
--- profesionales/views.py
+++ profesionales/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
-# Create your views here.
+
+from .forms import CerrajeroFormulario
+from .models import Cerrajero
+
+def crear_cerrajero(request):
+    
+    if request.method == "POST":
+        form = CerrajeroFormulario(request.POST)
+        
+        if form.is_valid():
+            data = form.cleaned_data
+            cerrajero = Cerrajero(nombre=data["nombre"],apellido=data["apellido"],desempleado=data["desempleado"])
+            cerrajero.save()
+            return redirect("index")
+            
+    form = CerrajeroFormulario()
+    return render (request,"profesionales/crear_cerrajero.html ", {'form': form})
+
