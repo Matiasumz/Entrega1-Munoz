e2a41514faef3c04305d6f72fc21ced7a5cbd179
Matiasumz <matiasumz96@gmail.com>
2022-04-13 20:58:41 -0300

    Se crearon las CBV para poder ver el detalle,editar y borrar cada profesional. Con sus respectivos botones de Volver

--- profesionales/urls.py
+++ profesionales/urls.py
@@ -5,10 +5,19 @@ from . import views
 urlpatterns = [
     path("cerrajeros/",views.lista_cerrajeros,name= "cerrajeros"),
     path("cerrajeros/crear/",views.crear_cerrajero, name= "crear_cerrajero"),
+    path("cerrajeros/<int:pk>/",views.DetalleCerrajero.as_view(), name= "detalle_cerrajero"),
+    path("cerrajeros/<int:pk>/editar",views.EditarCerrajero.as_view(), name= "editar_cerrajero"),
+    path("cerrajeros/<int:pk>/borrar",views.BorrarCerrajero.as_view(), name= "borrar_cerrajero"),
     path("futbolistas/",views.lista_futbolistas,name= "futbolistas"),
     path("futbolistas/crear/",views.crear_futbolista, name= "crear_futbolista"),
+    path("futbolistas/<int:pk>/",views.DetalleFutbolista.as_view(), name= "detalle_futbolista"),
+    path("futbolistas/<int:pk>/editar/",views.EditarFutbolista.as_view(), name= "editar_futbolista"),
+    path("futbolistas/<int:pk>/borrar/",views.BorrarFutbolista.as_view(), name= "borrar_futbolista"),
     path("profesores/",views.lista_profesores,name= "profesores"),
     path("profesor/crear/",views.crear_profesor, name= "crear_profesor"),
+    path("profesor/<int:pk>/",views.DetalleProfesor.as_view(), name= "detalle_profesor"),
+    path("profesor/<int:pk>/editar/",views.EditarProfesor.as_view(), name= "editar_profesor"),
+    path("profesor/<int:pk>/borrar/",views.BorrarProfesor.as_view(), name= "borrar_profesor"),
     
     #path("",name= "crear_profesor")
 ]
--- profesionales/views.py
+++ profesionales/views.py
@@ -3,6 +3,10 @@ from django.shortcuts import redirect, render
 
 from .forms import CerrajeroBusqueda , CerrajeroFormulario,FutbolistaFormulario,ProfesorFormulario, FutbolistaBusqueda , ProfesorBusqueda
 from .models import Cerrajero,Futbolista,Profesor
+from django.views.generic import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import UpdateView, DeleteView,CreateView
+
 
 def crear_cerrajero(request):
     
@@ -89,3 +93,49 @@ def lista_profesores(request):
   
     form = ProfesorBusqueda()
     return render(request,"profesionales/lista_profesores.html",{"form":form,"profesores":profesores})
+
+
+
+#Creo las clases basadas en vista con detalle,editar y borrar 
+
+class DetalleCerrajero(DetailView):
+    model = Cerrajero
+    template_name = "profesionales/detalle_cerrajero.html"
+    
+class EditarCerrajero(UpdateView):
+    model = Cerrajero
+    success_url ="/profesionales/cerrajeros"
+    fields =["nombre","apellido","desempleado"]
+    
+
+class BorrarCerrajero(DeleteView):
+    model = Cerrajero
+    success_url = "/profesionales/cerrajeros/"
+
+
+class DetalleFutbolista(DetailView):
+    model = Futbolista
+    template_name = "profesionales/detalle_futbolista.html"
+    
+class EditarFutbolista(UpdateView):
+    model = Futbolista
+    success_url = "/profesionales/futbolistas/"
+    fields=["nombre","apellido","club"]
+
+class BorrarFutbolista(DeleteView):
+    model = Futbolista
+    success_url = "/profesionales/futbolistas/"
+
+
+class DetalleProfesor(DetailView):
+    model = Profesor
+    template_name = "profesionales/detalle_profesor.html"
+    
+class EditarProfesor(UpdateView):
+    model = Profesor
+    success_url = "/profesionales/profesores/"
+    fields=["nombre","apellido","curso"]
+
+class BorrarProfesor(DeleteView):
+    model = Profesor
+    success_url = "/profesionales/profesores/"
