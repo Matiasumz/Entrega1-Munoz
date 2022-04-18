d6ca8a16f08ff47c73d7d5d39d7d7f1492c85578
Matiasumz <matiasumz96@gmail.com>
2022-04-17 23:47:31 -0300

    Se implementa la vista Registrarse con su respectivo formulario y el redireccionamiento hacia login

--- index/urls.py
+++ index/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import index , plantilla,login
+from .views import index , plantilla,login,registrarse
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path("",index, name="index"),
     path("login/",login, name="login"),
+    path("registrarse/",registrarse, name="registrarse"),
     path("logout/",LogoutView.as_view(template_name ="index\logout.html" ), name="logout"),
     path("plantilla/",plantilla,name="plantilla"),
 ]
--- index/views.py
+++ index/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms  import AuthenticationForm
 from django.contrib.auth import login as auth_login,authenticate
+from .forms import NuestroUserForm
+
+from index.forms import NuestroUserForm
 
 def index(request):
     return render(request,"index/index.html")
@@ -28,3 +31,23 @@ def login(request):
         
     login_form =  AuthenticationForm()
     return render(request,"index/login.html", {"login_form" : login_form})
+
+def registrarse(request):
+    
+    if request.method == "POST":
+        form = NuestroUserForm(request.POST)
+        
+        if form.is_valid():
+            username = form.cleaned_data["username"]
+            form.save()
+            #return render(request,"index/index.html",{"Alerta":f"Se creo correctamente el usuario {username}"})
+            return redirect("login")
+        else:
+            return render(request,"index/registrarse.html",{"form":form,"Alerta": "El formulario no es valido"})
+        
+            
+        
+    
+    
+    form = NuestroUserForm()
+    return render(request,"index/registrarse.html",{"form":form})
