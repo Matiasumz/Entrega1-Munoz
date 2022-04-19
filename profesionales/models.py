ec869c59824290cf7b8d39863f4bbf6b522e8c4c
Matiasumz <matiasumz96@gmail.com>
2022-04-18 23:24:22 -0300

    Se agrega a models el campo fecha_creacion, que permite saber la fecha en el que fue creado cada profesional

--- profesionales/models.py
+++ profesionales/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from ckeditor.fields import RichTextField
+from django.utils import timezone
 
 
 class Cerrajero(models.Model):
@@ -7,6 +8,7 @@ class Cerrajero(models.Model):
     apellido = models.CharField(max_length=20)
     desempleado = models.BooleanField()
     tarjeta_presentacion = RichTextField(blank=True,null=True)
+    fecha_creacion = models.DateTimeField(default=timezone.now)
     
     def __str__(self):
         return f"{self.nombre} {self.apellido}"
@@ -17,6 +19,7 @@ class Futbolista(models.Model):
     apellido = models.CharField(max_length=20)
     club = models.CharField(max_length=30)
     tarjeta_presentacion = RichTextField(blank=True,null=True)
+    fecha_creacion = models.DateTimeField(default=timezone.now)
     
     def __str__(self):
         return f"{self.nombre} {self.apellido}-{self.club}"
@@ -27,6 +30,7 @@ class Profesor(models.Model):
     apellido = models.CharField(max_length=20)
     curso = models.CharField(max_length=20)
     tarjeta_presentacion = RichTextField(blank=True,null=True)
+    fecha_creacion = models.DateTimeField(default=timezone.now)
     
     def __str__(self):
         return f"{self.nombre} {self.apellido}-{self.curso}"
